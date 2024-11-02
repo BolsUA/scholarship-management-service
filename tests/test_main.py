b7fc7e292a8272f1955144687c90b5759e560786
PauloMaced0 <paulomacedo@ua.pt>
2024-11-02 00:36:11 +0000

    submit repeated fields

--- tests/test_main.py
+++ tests/test_main.py
@@ -371,22 +371,22 @@ def test_upload_document_files(client):
     document_filename2 = "test_document2.txt"
 
     # Use MultiDict for data
-    data = {
-        'name': 'Test Scholarship with Documents',
-        'publisher': 'Test Publisher',
-        'type': 'Research Scholarship',
-        'document_template': 'True',
-        'document_template': 'True',
-        'document_required': 'True',
-        'document_required': 'False'
-    }
+    data = [
+        ('name', 'Test Scholarship with Documents'),
+        ('publisher', 'Test Publisher'),
+        ('type', 'Research Scholarship'),
+        ('document_template', 'True'),
+        ('document_template', 'True'),
+        ('document_required', 'True'),
+        ('document_required', 'False')
+    ]
 
     # Use a list of tuples for files
-    files = {
-        'edict_file': ('edict.txt', b"Edict content", 'text/plain'),
-        'document_file': (document_filename1, document_content1, 'text/plain'),
-        'document_file': (document_filename2, document_content2, 'text/plain')
-    }
+    files = [
+        ('edict_file', ('edict.txt', b"Edict content", 'text/plain')),
+        ('document_file', (document_filename1, document_content1, 'text/plain')),
+        ('document_file', (document_filename2, document_content2, 'text/plain'))
+    ]
 
     response = client.post("/proposals", data=data, files=files)
     assert response.status_code == 200, response.text
