f80c120071d09237444ec6ec1f09041defb81e80
PauloMaced0 <paulomacedo@ua.pt>
2024-11-02 00:28:43 +0000

    fix unpack files

--- tests/test_main.py
+++ tests/test_main.py
@@ -383,9 +383,9 @@ def test_upload_document_files(client):
 
     # Use a list of tuples for files
     files = {
-        'edict_file', ('edict.txt', b"Edict content", 'text/plain'),
-        'document_file', (document_filename1, document_content1, 'text/plain'),
-        'document_file', (document_filename2, document_content2, 'text/plain')
+        'edict_file': ('edict.txt', b"Edict content", 'text/plain'),
+        'document_file': (document_filename1, document_content1, 'text/plain'),
+        'document_file': (document_filename2, document_content2, 'text/plain')
     }
 
     response = client.post("/proposals", data=data, files=files)
